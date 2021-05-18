67ea9b61be079bedccb3088e0ec15cb0bc045586
rncongi <rncongi@gmail.com>
2021-05-18 19:00:21 -0400

    Users can now login

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -5,8 +5,8 @@ class Api::V1::AuthController < ApplicationController
     @user = User.find_by(username: user_login_params[:username])
     # @user.authenticate('password')
     if @user && @user.authenticate(user_login_params[:password])
-      @token = encode_token({ user_id: @user.id })
-      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :accepted
+      token = encode_token({ user_id: @user.id })
+      render json: { user: @user, jwt: @token }, status: :accepted
     else
       render json: { message: 'Invalid username or password' }, status: :unauthorized
     end
